18683700129e4d92bf3b1e76999bf54f8be882b5
garu <tamalroy74658@gmail.com>
2025-09-04 21:21:49 +0600

    project

--- index.js
+++ index.js
@@ -20,8 +20,38 @@ setuP.append(button);*/
 * setuP.after(button);
 * setuP.append(button);
 */
-function click() {
-  
-  document.querySelector('.box-area').style.height = '500px';
-  // document.querySelector('.box-area').innerText = `  Lorem ipsum dolor sit amet consectetur adipisicing elit. Totasitatibus molestiae similique!  `;
-};
+
+function press() {
+  document.getElementById('text').innerHTML = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde perspiciatis assumenda maiores perferendis? Veritatis incidunt perferendis et beatae asperiores hic.`;
+  document.querySelector('.box-area').style.height = 'auto';
+}
+
+// *  Add element
+
+function addLi(fruit) {
+  const li = document.createElement('li');
+  //*  li.innerHTML = `${fruit}`;
+  li.appendChild(document.createTextNode(fruit));
+  document.querySelector('ul').appendChild(li);
+} 
+addLi('mango');
+/*
+function addLi2() {
+  const li = document.createElement('li');
+  li.innerText = 'grape';
+  const setUp = document.querySelector('ul').appendChild(li);
+  //*  setUp.append(li);
+}
+addLi2();
+*/
+//?    Edit item
+let editLi = document.querySelector('li:first-child');
+/*
+* editLi.innerText = 'jackfruit';
+* editLi.textContent = 'jackfruit';
+* const editLi2 = document.createElement('li');editLi2.textContent = 'orange'; editLi.replaceWith(editLi2);
+*/
+/*
+? const editTag = document.querySelector('li:last-child');
+? editTag.outerHTML = '<p>mango</p>';
+*/
---
+++ project/project.js
@@ -0,0 +1,19 @@
+//!  case one
+/*
+? function change1()
+* { document.querySelector('.box').style.backgroundColor = '#1abc9c';
+* document.querySelector('.box1').style.border = '1px solid #fff'; }
+
+? function change2() 
+* { document.querySelector('.box').style.backgroundColor = '#f1c40f';
+ * document.querySelector('.box2').style.border = '1px solid #fff'; }
+? function change3() 
+*{document.querySelector('.box').style.backgroundColor = '#d35400';
+*  document.querySelector('.box3').style.border = '1px solid #fff'; }
+? function change4() 
+*  { document.querySelector('.box').style.backgroundColor = '#27ae60';
+*  document.querySelector('.box4').style.border = '1px solid #fff'; }
+? function change5()  
+*{ document.querySelector('.box').style.backgroundColor = '#2980b9';
+*  document.querySelector('.box5').style.border = '1px solid #fff'; }
+*/
