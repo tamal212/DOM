f0abf9438f7c7bdf286cf74097a0c570086f1823
garu <tamalroy74658@gmail.com>
2025-09-11 19:10:24 +0600

    fail

---
+++ Project_two/two.js
@@ -0,0 +1,22 @@
+'use strict';
+//let height = document.getElementById('height');
+let weidth = document.getElementById('weidth');
+
+let score = document.getElementById('score');
+
+
+
+let result = document.querySelector('.result');
+
+
+
+let button = document.getElementById('button');
+button.addEventListener('click', function () {
+  //let height = document.getElementById('height').value;
+  console.log('height');
+});
+
+// let hello = document.getElementById('hello');
+// hello.addEventListener('submit', function (e) {
+//   e.preventDefault();
+// });
