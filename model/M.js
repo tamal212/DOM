6cb1ba712cdcb679ba703ff828493f9ececde609
garu <tamalroy74658@gmail.com>
2025-09-12 19:26:38 +0600

    calculation

--- model/M.js
+++ model/M.js
@@ -4,21 +4,40 @@ let button = document.getElementById('button');
 let score = document.getElementById('score');
 let result = document.querySelector('.result');
 
+
+
+
 button.addEventListener('click', function () {
   let storedHeight =parseFloat( height.value);
   let storedWeight =parseFloat( weight.value);
   storedHeight = storedHeight / 100;
   let sqrHeight = storedHeight * storedHeight;
   let bmi = storedWeight / sqrHeight;
-
-  score.textContent = bmi.toFixed(2);
+  '#EA2027';
+score.textContent = bmi.toFixed(2);
   result.style.display = 'block';
-})
 
 
+
+  if (score.textContent < 18.6) {
+  score.style.backgroundColor = '#FFC312';
+  } else if  (score.textContent <24.9) {
+  score.style.backgroundColor = '#009432';
+  } else { score.style.backgroundColor = '#EA2027'; }}
+  
+
+);
 
 
 let form = document.getElementById('form');
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+form.addEventListener('submit', function (a) {
+  a.preventDefault();
+ 
 });
+
+
+function reset() {
+  height.value = '';
+  weight.value = '';
+  result.style.display = 'none';
+}
