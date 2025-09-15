4cf8a814fd6de77a8747ac3dd1c4b28ad5f9637e
garu <tamalroy74658@gmail.com>
2025-09-15 19:24:41 +0600

    auto photo

--- api/java.js
+++ api/java.js
@@ -1,6 +1,24 @@
-const imgItem = document.querySelector('#img_item');
-const API_url="https://picsum.photos/v2/list";
+const imgItem = document.getElementById("img_container");
+const API_url = 'https://picsum.photos/v2/list';
 const getImg = () => {
   fetch(API_url)
     .then(data => data.json())
+    .then(item => {
+      //console.log(item);
+      item.forEach(function (div) {
+        const newDiv = document.createElement('div');
+        newDiv.classList.add("img_item");
+
+        const newImg = document.createElement("img");
+        newImg.src = div.download_url;
+        newDiv.appendChild(newImg);
+
+        const newP = document.createElement("p");
+        newP.innerText = div.author;
+        newDiv.appendChild(newP);
+
+        imgItem.appendChild(newDiv);
+      })
+     })
 };
+getImg();
